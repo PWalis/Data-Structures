26851cbb6a1de84a5f716cca16b17dda57500b79
Brian Doyle <briandoyle81@gmail.com>
2019-09-25 17:40:11 -0600

    tryint to push to my branch

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,4 @@
+from doubly_linked_list import DoublyLinkedList
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +8,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.dictionary = {}
+        self.dll = DoublyLinkedList()
+        self.node_list = []
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +21,16 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if not key in self.dictionary.keys():
+            return None
+        else:
+            value = self.dictionary[key].value
+            node = self.dictionary[key]
+            if node in self.node_list:
+                self.dll.move_to_front(node)
+                return value
+            else:
+                return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +43,58 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        dll_len = self.dll.length
+        # if this is a new entry in the dictionary 
+        if key not in self.dictionary.keys():
+            if dll_len < self.limit:
+                # if there are less than 10 things in cache just add to dict and dll
+                self.dll.add_to_head(value)
+                node = self.dll.head
+                self.node_list.append(node)
+                self.dictionary.update({key:node})
+
+                # If limit is met remove the tail from the dll add the new value as head
+            elif dll_len >= self.limit:
+                self.dll.add_to_head(value)
+                tail = self.dll.tail
+                self.dll.remove_from_tail()
+
+                self.node_list.remove(tail)
+                node = self.dll.head
+                self.node_list.append(node)
+
+                self.dictionary.update({key:node})
+        # if this is not a new entry in the dictionary 
+        else:
+            node = self.dictionary[key]
+            # If an existing node in cache is being set again delete that node and create a new head node 
+            # with that new value but don't remove it from the node_list
+            if node in self.node_list:
+                node.delete()
+                self.node_list.remove(node)
+                self.dll.add_to_head(value)
+                new_node = self.dll.head
+                self.node_list.append(new_node)
+                self.dictionary.update({key:new_node})
+                
+            else:
+                self.dll.add_to_head(value)
+                tail = self.dll.tail
+                self.dll.remove_from_tail()
+
+                self.node_list.remove(tail)
+                node = self.dll.head
+                self.node_list.append(node)
+
+                self.dictionary.update({key:node})
+
+
+cache = LRUCache(3)
+
+cache.set('item1', 'a')
+cache.set('item2', 'b')
+cache.set('item3', 'c')
+cache.set('item2', 'z')
+print(cache.get('item1'))
+print(cache.get('item2'))
+print(len(cache.node_list))
